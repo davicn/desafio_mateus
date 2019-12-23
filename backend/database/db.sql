77c717cc2059a08edfa762c30c4e5719e9511e6a
davicn <davi.nascimento777@gmail.com>
2019-12-23 13:01:52 -0300

    Api funcinando!

--- backend/database/db.sql
+++ backend/database/db.sql
@@ -15,7 +15,6 @@ create table posts(
     primary key (idPost), 
     foreign key (idUser) references users(idUser));
 
-
 create table parceirias(
     idParceiro int not null auto_increment, 
     nome varchar(45) not null, 
@@ -36,8 +35,8 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345';
 -- insert em users
 insert into users (nome, local) values ('Fulano da Silva','Filial 2');
 
--- insert em parceirias
-insert into parceirias (nome,texto,image,telefone) values 
+-- insert em parcerias
+insert into parcerias (nome,texto,image,telefone) values 
 ("Parceiro 1","Essa parceiria é top.",'/home/davi/Documentos/teste_mateus/backend/database/img/img1.jpeg',"92345678"),
 ("Parceiro 2","Essa parceiria é show!",'/home/davi/Documentos/teste_mateus/backend/database/img/img2.jpeg',"987654321");
 /*
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,15 +4,10 @@ const routes = express.Router()
 const mysqlConnection = require('../database/conn.js');
 
 //select de parcerias
-routes.get('/parceirias', (req, res) => {
-    query = 'SELECT * FROM parceirias'
+routes.get('/parcerias', (req, res) => {
+    query = 'SELECT * FROM parcerias'
     mysqlConnection.query(query, (err, rows, fields) => {
-        if (!err) {
-            res.json(rows)
-            console.log(rows)
-        } else {
-            console.log(err)
-        }
+        if (err) throw err
     })
 })
 
@@ -20,55 +15,32 @@ routes.get('/parceirias', (req, res) => {
 routes.get('/avisos', (req, res) => {
     query = 'SELECT * FROM avisos'
     mysqlConnection.query(query, (err, rows, fields) => {
-        if (!err) {
-            res.json(rows)
-            console.log(rows)
-        } else {
-            console.log(err)
-        }
+        if (err) throw err
     })
 })
-
 
 // select de posts
 routes.get('/posts', (req, res) => {
     query = 'select nome,local,idPost,texto,imagem from users,posts where posts.idUser=users.idUser;'
     mysqlConnection.query(query, (err, rows, fields) => {
-        if (!err) {
-            res.json(rows)
-            console.log(rows)
-        } else {
-            console.log(err)
-        }
+        if (err) throw err
     })
 })
 
 // insert post
 routes.post('/post_post', (req, res) => {
-    let post_ = req.body
-    let texto = post_.texto
-    let imagem = post_.imagem
-    let idUser = '(select idUser from users)'
 
-    query = "insert into posts (texto,imagem,idUser) values ('" +
-        texto + "','" + imagem + "'," + idUser + ")"
+    query = "insert into posts values(null,'"
+        + req.body.texto + "','"
+        + req.body.imagem + "',"
+        + req.body.idUser + ")"
+    //console.log(query)
 
     mysqlConnection.query(query, (err, rows, fields) => {
-        if (!err) {
-            res.json(rows)
-            console.log(rows)
-        } else {
-            console.log(err)
-        }
+        if (err) throw err
     })
-
-    console.log(query)
-    return res.json({ Testando: "tranquilo" })
+    //return res.json({ Testando: "tranquilo" })
 
 })
-
-
-
-
 
 module.exports = routes 

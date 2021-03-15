ae29b46d4e5ece6d3f7e89becfe55262761f7e8d
Davi <davi.nascimento777@gmail.com>
2021-03-15 12:04:34 -0300

    Configurando basica de typescript

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+// const routes = require('./routes.js')
+
+const server = express()
+// server.use(express.json())
+// server.use(routes)
+
+
+server.listen(3000, () => {
+    console.log("Server On!")
+})
